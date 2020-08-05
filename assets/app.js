e53c8cba4b10299f6261a64f6cf83ff221c97515
Marina Lafuente <marlafsan@gmail.com>
2020-08-05 18:28:30 +0200

    routing

--- assets/app.js
+++ assets/app.js
@@ -1,64 +1,55 @@
 (function(){
 
+var app = angular.module ('myApp', ['ngRoute']);
 
-var app = angular.module ('myApp', []);
 app.controller('apiController', function($scope, $http){
 
-
-  // $scope.buscar = function(){
-  //   $http({
-  //     method: 'GET',
-  //     url: 'https://pokeapi.co/api/v2/pokemon/'
-  //     }).then(function successCallback(response, data, status, headers, config) {
-
-            // $http.get('https://pokeapi.co/api/v2/pokemon/'+ $scope.id)
-            // .then(function(res){
-            //   var pokemon = res.data.name;
-            //   $scope.pokemonNames.push(pokemon);
-            // });
-
+  $scope.buscar = function(){
+    $http({
+      method: 'GET',
+      url: 'https://pokeapi.co/api/v2/pokemon/'+ $scope.name
+      }).then(function successCallback(response, data, status, headers, config) {
 
           // $scope.pokemons.forEach(function(response, data){
-          //       $http.get('https://pokeapi.co/api/v2/pokemon/')
+          //       $http.get('https://pokeapi.co/api/v2/pokemon/'+ $scope.name)
           //            .then(function(response, data) {
           //                $scope.pokemons.push(response.data.name);
           //             });
           //     });
-        
+          $scope.name = response.data.name;
+          $scope.id = response.data.id;
+          $scope.image = response.data.sprites.front_default;
+          $scope.type = response.data.types.type;
 
-          // $scope.name = response.data.name;
-          // $scope.id = response.data.id;
-          // $scope.orden = response.data.base_experience;
-          // $scope.heigt = response.data.height;
-          // $scope.image = response.data.sprites.front_default;
 
-  //     }, function errorCallback(response, data, status, headers, config) {
-  //      alert("This pokemon is unknown even at his home");
-  //   });
+      }, function errorCallback(response, data, status, headers, config) {
+       alert("This pokemon is unknown even at his home");
+    });
 
-  // };
+  };
 
-  function randomInteger(min, max) {
+});
+
+//showing random pokemon every time the page is reloaded
+
+app.controller('randomController', function($scope, $http){
+
+function randomInteger(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-
   
     $scope.pokemons = [];
-    
-    /////////////
     
     function getPokeEndpoints(){
       var i;
       var endpoints = [];
-      for (i = 0; i < 9; i++){
+      for (i = 0; i < 12; i++){
         var random = randomInteger(1, 960); 
         var endPoint = 'https://pokeapi.co/api/v2/pokemon/' + random;
         endpoints.push(endPoint);
       }
       return endpoints;
     }
-    
-    ///////////// 
     
     function getStats(endPoint){
         $http.get(endPoint)
@@ -67,46 +58,31 @@ app.controller('apiController', function($scope, $http){
             
         });
     }
-    
-    /////////////
-    
+
     function getPokemons(endpoints){
       var i;
       for (i = 0; i < endpoints.length; i++){
         getStats(endpoints[i]);
       }
     }
-    
-    /////////////
-    
 
     getPokemons(getPokeEndpoints());
 
-  
 
 });
 
 
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/pokemon/:id', {
+      templateUrl: '/templates/card.html',
+    })
+    .otherwise({
+      redirectTo: '/index.html'
+    });
+});
 
 
-
-// app.directive('productTitle', function(){
-// 	return {
-// 		restrict: 'AE',
-// 		templateUrl: 'templates/product-title.html'
-// 	};
-// });
-
-// app.directive('productPanel', function(){
-// 		return{
-// 			restrict: 'AE',
-// 			templateUrl: 'templates/product-panel.html',
-// 			controller: function(){
-
-// 			},
-			
-// 		};
-// });
 
 
 })();
